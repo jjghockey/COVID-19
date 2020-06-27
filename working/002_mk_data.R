e3c6f8677de7f56896645ae5abf8cf64870b6b49
jjghockey <jjghockey@gmail.com>
2020-06-26 17:25:19 -0700

    Update 002_mk_data.R

--- working/002_mk_data.R
+++ working/002_mk_data.R
@@ -1,7 +1,7 @@
 #####################################################################################
 #	Engagement: COVID-19 Analysis       	                          							    #
 #	Program: 	002_mk_data_us_cln.r                        						          	    #		
-#	Last Update Date: 5/3/2020                                          							#
+#	Last Update Date: 6/26/2020                                          							#
 #																	                                        			    #
 #	Purpose: Make a clean file of the US                                              #
 #																                                          					#
